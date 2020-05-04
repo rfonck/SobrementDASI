22e3e0160dd38b4faf8c90466130688cff51a7b0
jj1999 <jjacques.meldrum@gmail.com>
2020-05-04 15:19:32 +0200

    Webapp

--- tp1/target/generated-sources/annotations/fr/insalyon/dasi/metier/modele/Utilisateur_.java
+++ tp1/target/generated-sources/annotations/fr/insalyon/dasi/metier/modele/Utilisateur_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-04-16T15:37:29")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-04T15:13:01")
 @StaticMetamodel(Utilisateur.class)
 public class Utilisateur_ { 
 
