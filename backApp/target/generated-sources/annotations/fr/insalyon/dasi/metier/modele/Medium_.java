e3be83ca3bd8685220abf4cc29f7697ba312923a
jj1999 <jjacques.meldrum@gmail.com>
2020-05-18 16:12:51 +0200

    Modif

--- backApp/target/generated-sources/annotations/fr/insalyon/dasi/metier/modele/Medium_.java
+++ backApp/target/generated-sources/annotations/fr/insalyon/dasi/metier/modele/Medium_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-11T17:56:26")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-18T14:41:11")
 @StaticMetamodel(Medium.class)
 public class Medium_ { 
 
--- webApp/src/main/java/fr/insalyon/dasi/ihm/web/serialisation/AfficherListeMediumSerialisation.java
+++ webApp/src/main/java/fr/insalyon/dasi/ihm/web/serialisation/AfficherListeMediumSerialisation.java
@@ -9,12 +9,16 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import fr.insalyon.dasi.metier.modele.Astrologue;
+import fr.insalyon.dasi.metier.modele.Cartomancien;
 import fr.insalyon.dasi.metier.modele.Medium;
+import fr.insalyon.dasi.metier.modele.Spirite;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 /**
  *
@@ -35,10 +39,31 @@ public class AfficherListeMediumSerialisation extends Serialisation {
             JsonObject medium = new JsonObject();
             medium.addProperty("sexe", listemedium.get(i).getSexe());
             medium.addProperty("denomination", listemedium.get(i).getDenomination());
+            if(listemedium.get(i) instanceof Cartomancien){
+                Cartomancien carto = (Cartomancien) listemedium.get(i);
+                medium.addProperty("type", "cartomancien");
+                medium.addProperty("presentation", carto.getPresentation());
+            }
+            else if(listemedium.get(i) instanceof Astrologue){
+                Astrologue carto = (Astrologue) listemedium.get(i);
+                medium.addProperty("type", "astrologue");
+                medium.addProperty("presentation", carto.getPresentation());
+                medium.addProperty("formation", carto.getFormation());
+                medium.addProperty("promotion", carto.getPromotion());
+            }
+            else if(listemedium.get(i) instanceof Spirite){
+                Spirite carto = (Spirite) listemedium.get(i);
+                medium.addProperty("type", "spirite");
+                medium.addProperty("presentation", carto.getPresentation());
+                medium.addProperty("support", carto.getSupport());
+            }
+            else{
+                medium.addProperty("type", "none");
+            }
             liste.add( medium);
         }
         container.add("listemedium", liste);
-
+        
         response.setContentType("application/json;charset=UTF-8");
         PrintWriter out = response.getWriter();
         Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
